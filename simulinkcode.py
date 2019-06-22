b35e5835b70b9d663c05d202af55d67f1fafd982
ogata <ogata@ait.kyushu-u.ac.jp>
2019-06-22 16:24:39 +0900

    completed code of collect slx's files

--- simulinkcode.py
+++ simulinkcode.py
@@ -1,37 +1,62 @@
 import requests as rq
 import subprocess as sp
-url="https://api.github.com/search/repositories?l=MATLAB&q=simulink"
-clonekey=[]
-def keyget(key):
+import glob
+class gitcollect:
 
-    error=0
-    a=rq.get(url).json()
-    totalcount=a["total_count"]
-    repeatcount=int(totalcount/30)
-    a=[]
+    def __init__(self,url="https://api.github.com/search/repositories?l=MATLAB&q=simulink"):
+        self.clonekey=[]
+        self.movelist=glob.glob("./**/*.slx",recursive=True)
+        self.url=url
+    def keyget(self,page=0):
+        error=0
+        flag=0
+        a=rq.get(self.url).json()
+        try:
+            totalcount=a["total_count"]
+        except KeyError:
+            print("API LIMITED")
+            flag=1
+        if(flag!=1):
+            repeatcount=int(totalcount/30)
+            a=[]
 
-    for i in range(repeatcount):
-        a.append(rq.get(url+"&page=%d",i+1).json())
-    for i in range(10):
-        for j in range(30):
-            print("i:"+str(i)+"j;"+str(j))
-            try:
-                key.append(a[i]["items"][j]["html_url"]+".git")
-            except KeyError:
-                print("Keyerror")
-                error+=1
-                print(a[i]["items"][j]["name"])
-    print("error"+str(error))
+            for i in range(page,page+10):
+                a.append(rq.get(self.url+"&page=%d",i+1).json())
+            for i in range(10):
+                for j in range(30):
+                    print("i:"+str(i)+"j;"+str(j))
+                    try:
+                        self.clonekey.append(a[i]["items"][j]["html_url"]+".git")
+                    except KeyError:
+                        print("Keyerror")
+                        error+=1
+                    except IndexError:
+                        print("out of range")
+                        error+=1
+            print("error"+str(error))
 
-def clone(key):
-    for i in key:
-        cmd="git clone "+i+" ./"
-        sp.call(cmd.split())
+    def clone(self):
+        j=0
+        for i in self.clonekey:
+            cmd="git clone "+i+" ./test/sample"+str(j)
+            sp.call(cmd.split())
 
-def main(key):
-    keyget(key)
-    clone(key)
-    print("finish")
+    def move(self):
+        for i in self.movelist:
+            cmd="mv "+i+" ./test/"
+            sp.call(cmd.split())
+
+    def main(self):
+        sp.call("mkdir test".split())
+        for i in range(10):
+            self.keyget(i*10)
+            print("keyget time"+str(i))
+
+        self.clone()
+        self.move()
+        print("finish")
+        # self.move()
 
 if __name__ == '__main__':
-    main(clonekey)
+    a=gitcollect()
+    a.main()
